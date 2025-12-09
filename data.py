0686df1464575ac02e84c01e18933270066058fc
shanemanares <shanemanares16@gmail.com>
2025-12-09 16:54:21 +0800

    Add files via upload
    Questions, Option, Answer

---
+++ data.py
@@ -0,0 +1,282 @@
+data = {
+    "What is a Linked List?": {
+        "options": [
+            "A data structure with fixed size",
+            "A collection of nodes connected by pointers",
+            "A type of array",
+            "A function that sorts data"
+        ],
+        "answer": "A collection of nodes connected by pointers"
+    },
+    "What does each node in a linked list usually contain?": {
+        "options": [
+            "Only data",
+            "A pointer only",
+            "Data and a pointer to the next node",
+            "A function that sorts data"
+        ],
+        "answer": "Data and a pointer to the next node"
+    },
+    "Which linked list allows traversal in both directions?": {
+        "options": [
+            "Singly linked list",
+            "Doubly linked list",
+            "Circular linked list",
+            "Linear list"
+        ],
+        "answer": "Doubly linked list"
+    },
+    "What is the first node of a linked list called?": {
+        "options": ["Head", "Root", "Start", "Base"],
+        "answer": "Head"
+    },
+    "Which is an advantage of linked lists over arrays?": {
+        "options": [
+            "Faster random access",
+            "Fixed memory size",
+            "Dynamic memory allocation",
+            "No need for pointers"
+        ],
+        "answer": "Dynamic memory allocation"
+    },
+    "A stack follows which principle?": {
+        "options": ["FIFO", "LIFO", "LILO", "FILO"],
+        "answer": "LIFO"
+    },
+    "Which operation inserts an element into a stack?": {
+        "options": ["Insert", "Enqueue", "Push", "Add"],
+        "answer": "Push"
+    },
+    "Which operation removes the top element of a stack?": {
+        "options": ["Pop", "Delete", "Remove", "Dequeue"],
+        "answer": "Pop"
+    },
+    "Where can elements be inserted or removed in a stack?": {
+        "options": ["Both ends", "Middle only", "Rear only", "Top only"],
+        "answer": "Top only"
+    },
+    "What happens when you pop from an empty stack?": {
+        "options": ["Stack Full", "Underflow", "Overflow", "Crash"],
+        "answer": "Underflow"
+    },
+    "A queue follows which principle?": {
+        "options": ["LIFO", "Random", "FIFO", "LILO"],
+        "answer": "FIFO"
+    },
+    "Which operation inserts an element into a queue?": {
+        "options": ["Push", "Append", "Enqueue", "Pop"],
+        "answer": "Enqueue"
+    },
+    "Which operation removes an element from a queue?": {
+        "options": ["Remove", "Pop", "Dequeue", "Delete"],
+        "answer": "Dequeue"
+    },
+    "In a standard queue, new elements enter at the ____?": {
+        "options": ["Front", "Rear", "Middle", "Top"],
+        "answer": "Rear"
+    },
+    "In a queue, which element is removed first?": {
+        "options": [
+            "The newest element",
+            "The oldest element",
+            "Any random element",
+            "The middle element"
+        ],
+        "answer": "The oldest element"
+    },
+    "What type of queue allows inserting at the rear and deleting at the front?": {
+        "options": [
+            "Circular queue",
+            "Double-ended queue",
+            "Simple queue",
+            "Priority queue"
+        ],
+        "answer": "Circular queue"
+    },
+    "Which data structure is often used to implement recursion?": {
+        "options": ["Queue", "Tree", "Stack", "Array"],
+        "answer": "Stack"
+    },
+    "What is a circular linked list?": {
+        "options": [
+            "A list where the last node points back to the first",
+            "A list that forms a loop in memory",
+            "A list with no head",
+            "A list that uses arrays"
+        ],
+        "answer": "A list where the last node points back to the first"
+    },
+    "What happens during queue overflow?": {
+        "options": [
+            "Removing from an empty queue",
+            "Adding to a full queue",
+            "The queue resets",
+            "Pointers get deleted"
+        ],
+        "answer": "Adding to a full queue"
+    },
+    "Which data structure is best for scheduling tasks in order?": {
+        "options": ["Stack", "Queue", "Linked List", "Tree"],
+        "answer": "Queue"
+    },
+    "Which of the following is a selection control structure?":{
+         "options": [ "for loop", "if-else", "while loop", "do-while"],
+         "answer": "if-else"
+    },
+    "The condition inside an if statement must evaluate to:":{
+         "options": ["char", "string", "boolean", "integer"],
+         "answer": "boolean"
+    },
+    "What does a loop do?":{
+         "options": ["Runs code once", "Repeats code", "Deletes variables", "Ends a program"],
+         "answer": "Repeats code"
+    },
+    "Which loop is guaranteed to run at least once?":{
+          "options": ["for", "while", "do-while", "none"],
+           "answer": "do-while"
+    },
+    "What keyword is used to exit a loop early?":{
+           "options": ["end", "stop", "break", "exit"],
+           "answer":  "break"
+    },
+    "What keyword skips to the next loop iteration?":{
+          "options": [ "break", "skip", "continue", "pass"],
+           "answer":  "continue"
+    },
+    "A nested loop means:":{
+          "options": [ "Two loops running separately", "One loop inside another", "A loop inside a method", "A loop with no condition"],
+            "answer":  "One loop inside another"
+    },
+    "An infinite loop happens when:":{
+           "options": [ "The loop has no body", "The condition is always true", "The condition is always false", "The loop uses variables"],
+            "answer":  "The condition is always true"
+    },
+    "Which of these is NOT a logical operator?":{
+            "options": [ "&&", "||", "!", "=="],
+            "answer":  "=="
+    },
+    "Which structure is best for multiple specific choices?":{
+            "options": ["if", "if-else", "nested if", "switch"],
+            "answer":  "switch"
+    },
+    "What is an array?":{
+            "options": [ "A single variable", "A collection of variables of the same type", "A method", "A loop"],
+             "answer":  "A collection of variables of the same type"
+    },
+    " Array indexing starts at:":{
+            "options": [ "-1", "0", "1", "10"],
+            "answer":  "0"
+    },
+    "What is the length of this array: int[] x = {2, 4, 6, 8};":{
+             "options": ["2", "3", "4", "8"],
+             "answer":  "4"
+    },
+    "Accessing array[10] in a size-5 array causes:":{
+            "options": [ "Nothing", "Compilation success", "Out of bounds error", "Infinite loop"],
+            "answer":  "Out of bounds error"
+    },
+    "What loop is often used to iterate through arrays?":{
+             "options": ["for", "if", "switch", "break"],
+             "answer":  "for"
+    },
+    " What does array.length return?":{
+            "options": ["Last index", "Memory size", "Number of elements", "Data type"],
+             "answer":  "Last index"
+    },
+    "A 2D array is also known as a:":{
+            "options": ["Matrix", "Queue", "Stack", "Method"],
+            "answer":  "Stack"
+    },
+    "Which is a valid array declaration?":{
+            "options": [" int arr;", "int arr[];", "arr int[];", "array int[];"],
+            "answer":  "int arr;"
+    },
+    "What is the last index of an array with length 10?":{
+            "options": ["10", "9", "5", "0"],
+            "answer":  "9"
+    },
+    " An array can store:":{
+            "options": ["Multiple data types", "Only one data type", "Only integers","Only strings"],
+            "answer":  "Only one data type"
+    },
+    "Which loop is best when the number of repetitions is known?":{
+            "options": ["while", "do-while", "for", "switch"],
+            "answer":   "for"
+    },
+    "What does i++ mean?":{
+            "options": ["decrease i", "increase i by 1", "multiply i", "reset i"],
+            "answer":  "increase i by 1"
+    },
+    "A for loop has how many parts in its header?":{
+            "options": [" 1", "2", "3", "4"],
+            "answer":  "3"
+    },
+    "What happens if you don’t update the loop variable?":{
+            "options": ["The loop runs normally", "It becomes an infinite loop", "It becomes a nested loop", "It becomes a recursion"],
+            "answer":  "It becomes an infinite loop"
+    },
+    "Which loop checks the condition at the end?":{
+            "options": [" while", "for", "switch", "do-while"],
+             "answer":  "do-while"
+    },
+    "What is a method?":{
+            "options": ["A loop", "A reusable block of code", "A variable", "An array"],
+            "answer":  "A reusable block of code"
+    },
+    "A method that returns nothing uses:":{
+            "options": [" int", "return", "void", "empty"],
+             "answer":   "void"
+    },
+    " What is a parameter?":{
+           "options": [" A value returned by a method", "A variable inside a method", "A value passed into a method", "The method name"],
+           "answer":  "A value passed into a method"
+    },
+    "Which keyword is used to return a value?":{
+           "options": [" break", "exit", "return", "send"],
+           "answer":  "return"
+    },
+    "A method must end with:":{
+            "options": [" stop", "}", "return", "exit"],
+            "answer":  "}"
+    },
+    "Methods help make code:":{
+            "options": ["Longer", "More repetitive", "Modular and reusable", "Useless"],
+            "answer":  "Modular and reusable"
+    },
+    "Overloading a method means:":{
+            "options": ["Using the same name but different parameters", "Writing too many methods", "Calling a method inside itself", "Returning multiple results"],
+            "answer":  "Using the same name but different parameters"
+    },
+    "A method call is:":{
+            "options": ["Creating a method", "Running/executing a method", "Deleting a method", "Editing a method"],
+             "answer":  "Deleting a method"
+    },
+    "What is recursion?":{
+            "options": [" A loop", "A method that calls itself", "An array process", "A compiler error"],
+            "answer":  "An array process"
+    },
+    "A recursive method must have:":{
+            "options": ["unlimited calls", "no return", "a base case", "a loop"],
+            "answer":  "a base case"
+    },
+    "Without a base case, recursion becomes:":{
+            "options": ["simple", "infinite", "optional", "safe"],
+            "answer":  "infinite"
+    },
+    "Which is an example of a base case?":{
+            "options": ["if (n == 1) return 1;", "n = n + 1;", "print(n);", "call method again"],
+             "answer":  "if (n == 1) return 1;"
+    },
+    "Recursion is good for solving:":{
+            "options": [" Repetitive tasks", "Problems that can be broken into smaller subproblems", "Data entry", "Keyboard input"],
+            "answer":  "Problems that can be broken into smaller subproblems"
+    },
+    "Which is NOT an example of a common recursive algorithm?":{
+            "options": [" Factorial", "Fibonacci", "Binary search", "Printing Hello World once"],
+            "answer":   "Printing Hello World once"
+    },
+    "Recursion eventually reduces a problem until it becomes:":{
+            "options": ["bigger", "unsolvable", "the base case", "an array"],
+             "answer":  "the base case"
+    }
+    }
